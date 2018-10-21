8f7ef95fef18c209aab507e640bc38cb669d080f
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-21 16:03:40 +0200

    Dropdown menu works!

--- js/app.js
+++ js/app.js
@@ -2,14 +2,30 @@ const triggers = document.querySelectorAll('.cool > li');
 const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
 
-function handleEnter(event) {
+function handleEnter() {
     this.classList.add('trigger-enter');
-    setTimeout(()=> this.classList.add('trigger-enter-active'), 120);
+    setTimeout(()=> this.classList.contains('trigger-enter')&& this.classList.add('trigger-enter-active'), 130);
+    background.classList.add('open');
+    const dropdown = this.querySelector('.dropdown');
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+    
+    const coords = {
+        height: dropdownCoords.height,
+        width: dropdownCoords.width,
+        top: dropdownCoords.top - navCoords.top,
+        left: dropdownCoords.left - navCoords.left
+    }
+    
+    background.style.height = `${coords.height}px`;
+    background.style.width = `${coords.width}px`;
+    background.style.top = `${coords.top}px`;
+    background.style.left = `${coords.left}px`; 
 }
 
-function handleLeave(event) {
+function handleLeave() {
     this.classList.remove('trigger-enter', 'trigger-enter-active');
-
+    background.classList.remove('open');
 }
 
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
