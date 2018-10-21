d4907e8847cfe3c1fdb503be13a763a850e8b828
Dominika Szukała <dominika.szukala@gmail.com>
2018-10-21 13:17:24 +0200

    Dropdown list appears

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 const triggers = document.querySelectorAll('.cool > li');
 const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('.top');
+
+function handleEnter(event) {
+    this.classList.add('trigger-enter', 'trigger-enter-active');
+}
+
+function handleLeave(event) {
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
+
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
